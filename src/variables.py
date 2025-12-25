4128b560bfef576598b99dfcd96dfeddd2bc6e83
Ali Meirbekov <ameirbekov09@gmail.com>
2025-12-25 11:18:27 +0500

    Create variables.py

---
+++ src/variables.py
@@ -0,0 +1,11 @@
+name = "Ali"
+age = 15
+height_cm = 180
+
+print("Name:", name)
+print("Age:", age)
+print("Height (cm):", height_cm)
+
+# Simple calculations
+years_until_18 = 18 - age
+print("Years until 18:", years_until_18)
