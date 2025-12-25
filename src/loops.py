66bbc1afcfa3b95d35c95248061bdc321b730957
Ali Meirbekov <ameirbekov09@gmail.com>
2025-12-25 11:19:33 +0500

    Add loop examples in loops.py

---
+++ src/loops.py
@@ -0,0 +1,14 @@
+numbers = [3, 7, 2, 9, 4]
+total = 0
+
+for n in numbers:
+    total += n
+
+print("Numbers:", numbers)
+print("Sum:", total)
+
+# While loop example
+x = 5
+while x > 0:
+    print("Countdown:", x)
+    x -= 1
